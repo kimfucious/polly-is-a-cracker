d8974425acd30c9c13793790aad53f0025fc059c
Kimfucious <github@kimford.me>
2019-08-21 15:26:43 +0700

    unsubscribe on destroy.  delete unused code.

--- src/app/components/polly-joke/polly-joke.component.ts
+++ src/app/components/polly-joke/polly-joke.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, HostListener } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 import { JokeService } from "src/app/services/joke.service";
 import { Joke } from "src/app/models/Joke";
 import { SpeechParams } from "src/app/models/SpeechParams";
 import { Subscription } from "rxjs";
 import { config, CognitoIdentityCredentials, Polly } from "aws-sdk";
 import { Alert } from "../../models/Alert";
+
 config.region = "us-west-2";
 config.credentials = new CognitoIdentityCredentials({
   IdentityPoolId: "us-west-2:ff9b3fd3-e705-489e-a20f-62a22f58b1b0"
@@ -15,7 +16,7 @@ config.credentials = new CognitoIdentityCredentials({
   templateUrl: "./polly-joke.component.html",
   styleUrls: ["./polly-joke.component.scss"]
 })
-export class PollyJokeComponent implements OnInit {
+export class PollyJokeComponent implements OnInit, OnDestroy {
   alerts: Alert[];
   audio = new Audio();
   audioSource: string;
@@ -26,7 +27,9 @@ export class PollyJokeComponent implements OnInit {
   subscription: Subscription;
 
   constructor(private jokeService: JokeService) {}
-
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
   ngOnInit() {
     this.speechParams = {
       Engine: "neural",
@@ -68,7 +71,6 @@ export class PollyJokeComponent implements OnInit {
     this.alerts = [];
   }
   speakText() {
-    const polly = new Polly({ apiVersion: "2016-06-10" });
     const signer = new Polly.Presigner();
 
     signer.getSynthesizeSpeechUrl(
