1da902cf76a73a0e300ad5bc51b07940eee5db32
Kimfucious <github@kimford.me>
2019-08-21 15:24:11 +0700

    move to shareable location

---
+++ src/app/data/voices.ts
@@ -0,0 +1,20 @@
+export const voices = [
+  "Aditi",
+  "Brian",
+  "Carla",
+  "Carmen",
+  "Celine",
+  "Conchita",
+  "Emma",
+  "Hans",
+  "Ines",
+  "Joanna",
+  "Mathieu",
+  "Matthew",
+  "Mizuki",
+  "Nicole",
+  "Raveena",
+  "Russell",
+  "Seoyeon",
+  "Vicki"
+];
